88c87ca5a6ea0638cf6d0897403a28616995fbac
flaminana <hannahazmir05@gmail.com>
2025-11-26 23:53:35 +0800

    Update the tts.py code

--- utils/tts.py
+++ utils/tts.py
@@ -2,11 +2,20 @@ import subprocess
 import os
 
 def synthesize_audio(text: str, output_path: str, direction: str):
+    #Base path relative to this file
+
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    model_dir = os.path.join(base_dir, "../models/piper")
+
     # Choose model based on direction
     if direction == "ENG-GER":
         model_path = "models/piper/de_DE-karlsson-low.onnx"
     else:
         model_path = "models/piper/en_US-amy-low.onnx"
+
+    if not os.path.exists(model_path):
+        print(f"❌ Model file not found at {model_path}")
+        raise FileNotFoundError(f"Model file not found at {model_path}")
 
     command = [
         "piper",
